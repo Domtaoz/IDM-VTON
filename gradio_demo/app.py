29a42f7ec76bade603d48f2508a6182ebd71d955
Domtaoz <domdypol@gmail.com>
2025-05-29 00:53:13 +0530

    interface

--- gradio_demo/app.py
+++ gradio_demo/app.py
@@ -256,7 +256,6 @@ for ex_human in human_list_path:
     human_ex_list.append(ex_dict)
 
 
-
 iface = gr.Interface(
     fn=start_tryon,    # ฟังก์ชันหลักที่ประมวลผล (ต้องรับ parameter ตรงกับ inputs)
     inputs=[
@@ -275,63 +274,10 @@ iface = gr.Interface(
     allow_flagging="never",
     live=False,
     title="IDM-VTON Try-On API",
-    description="Try-On REST API for Virtual Try-On project.",
-    # show_api=True   
+    description="Try-On REST API for Virtual Try-On project."
 )
 
 iface.launch(share=True)
 
 
 
-# image_blocks = gr.Blocks().queue()
-# with image_blocks as demo:
-#     gr.Markdown("## IDM-VTON 👕👔👚")
-#     gr.Markdown("Virtual Try-on with your image and garment image. Check out the [source codes](https://github.com/yisol/IDM-VTON) and the [model](https://huggingface.co/yisol/IDM-VTON)")
-#     with gr.Row():
-#         with gr.Column():
-#             imgs = gr.ImageEditor(sources='upload', type="pil", label='Human. Mask with pen or use auto-masking', interactive=True)
-#             with gr.Row():
-#                 is_checked = gr.Checkbox(label="Yes", info="Use auto-generated mask (Takes 5 seconds)",value=True)
-#             with gr.Row():
-#                 is_checked_crop = gr.Checkbox(label="Yes", info="Use auto-crop & resizing",value=False)
-
-#             example = gr.Examples(
-#                 inputs=imgs,
-#                 examples_per_page=10,
-#                 examples=human_ex_list
-#             )
-
-#         with gr.Column():
-#             garm_img = gr.Image(label="Garment", sources='upload', type="pil")
-#             with gr.Row(elem_id="prompt-container"):
-#                 with gr.Row():
-#                     prompt = gr.Textbox(placeholder="Description of garment ex) Short Sleeve Round Neck T-shirts", show_label=False, elem_id="prompt")
-#             example = gr.Examples(
-#                 inputs=garm_img,
-#                 examples_per_page=8,
-#                 examples=garm_list_path)
-#         with gr.Column():
-#             # image_out = gr.Image(label="Output", elem_id="output-img", height=400)
-#             masked_img = gr.Image(label="Masked image output", elem_id="masked-img",show_share_button=False)
-#         with gr.Column():
-#             # image_out = gr.Image(label="Output", elem_id="output-img", height=400)
-#             image_out = gr.Image(label="Output", elem_id="output-img",show_share_button=False)
-
-
-
-
-#     with gr.Column():
-#         try_button = gr.Button(value="Try-on")
-#         with gr.Accordion(label="Advanced Settings", open=False):
-#             with gr.Row():
-#                 denoise_steps = gr.Number(label="Denoising Steps", minimum=20, maximum=40, value=30, step=1)
-#                 seed = gr.Number(label="Seed", minimum=-1, maximum=2147483647, step=1, value=42)
-
-
-
-#     try_button.click(fn=start_tryon, inputs=[imgs, garm_img, prompt, is_checked,is_checked_crop, denoise_steps, seed], outputs=[image_out,masked_img], api_name='tryon')
-
-            
-
-
-# image_blocks.launch(share=True)
